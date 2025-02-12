88553f1310ec5e8cb92f6425a2ea0b05101f3656
thulasidharan96 <thulasidharankvp@gmail.com>
2025-02-12 14:53:47 +0530

    Api

--- src/service/Api.js
+++ src/service/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-const LOGIN_URL = 'https://rest-api-69mq.onrender.com/user/login';
+const LOGIN_URL = 'https://rest-api-hp0n.onrender.com/user/login';
 
 // Login API
 
@@ -9,7 +9,7 @@ export const LoginApi = async (data) => {
     return await axios.post(LOGIN_URL, data);
 };
 
-const REGISTER_URL = 'https://rest-api-69mq.onrender.com/user/signup';
+const REGISTER_URL = 'https://rest-api-hp0n.onrender.com/user/signup';
 
 // Register API
 export const RegisterApi = async (data) => {
@@ -18,7 +18,7 @@ export const RegisterApi = async (data) => {
 
 // get attendance status API by userId
 export const UserApi = async (userIdd) => {
-    const USER_URL = `https://rest-api-69mq.onrender.com/attendance/${userIdd}`;
+    const USER_URL = `https://rest-api-hp0n.onrender.com/attendance/${userIdd}`;
     const token = localStorage.getItem('authToken');
     return await axios.get(USER_URL, {
         headers: {
@@ -35,7 +35,7 @@ export const AttendanceApi = async (data) => {
     const name = localStorage.getItem('name');
     const department = localStorage.getItem('department');
     const registerNumber = localStorage.getItem('RegisterNumber');
-    const ATTENDANCE_URL = 'https://rest-api-69mq.onrender.com/attendance/';
+    const ATTENDANCE_URL = 'https://rest-api-hp0n.onrender.com/attendance/';
 
     if (!token) throw new Error("Authentication token missing");
     if (!userId) throw new Error("User ID missing");
@@ -72,7 +72,7 @@ export const AttendanceApi = async (data) => {
 export const CurrentAttendanceByDate = async () => {
   const token = localStorage.getItem('authToken');
   const userId = localStorage.getItem('userId');
-  const CURRENT_ATTENDANCE_URL = 'https://rest-api-69mq.onrender.com/admin';
+  const CURRENT_ATTENDANCE_URL = 'https://rest-api-hp0n.onrender.com/admin';
 
   // Validate that both userId and token are available
   if (!token) throw new Error("Authentication token missing");
@@ -94,7 +94,7 @@ export const CurrentAttendanceByDate = async () => {
 
 //Student  Attendance by register number
 export const studentbyRegisterNo = async (registrationNumber) => {
-  const USER_URL = `https://rest-api-69mq.onrender.com/admin/${registrationNumber}`;
+  const USER_URL = `https://rest-api-hp0n.onrender.com/admin/${registrationNumber}`;
   const token = localStorage.getItem('authToken');
   return await axios.get(USER_URL, {
     headers: {
@@ -106,7 +106,7 @@ export const studentbyRegisterNo = async (registrationNumber) => {
 
 // Department Attendance Report API
 export const getDepartmentReport = async (department) => {
-  const USER_URL = `https://rest-api-69mq.onrender.com/admin/department/${department}`;
+  const USER_URL = `https://rest-api-hp0n.onrender.com/admin/department/${department}`;
   const token = localStorage.getItem('authToken');
   return await axios.get(USER_URL, {
     headers: {
@@ -118,7 +118,7 @@ export const getDepartmentReport = async (department) => {
 
 // Date Range Attendance Report API
 export const allData = async () => {
-  const USER_URL = `https://rest-api-69mq.onrender.com/admin/all`;
+  const USER_URL = `https://rest-api-hp0n.onrender.com/admin/all`;
   const token = localStorage.getItem('authToken');
   return await axios.get(USER_URL, {
     headers: {
@@ -130,7 +130,7 @@ export const allData = async () => {
 
 // Search User by UserId API
 export const searchUserByUserId = async (userId) => {
-  const API_URL = 'https://rest-api-69mq.onrender.com/admin/';
+  const API_URL = 'https://rest-api-hp0n.onrender.com/admin/';
   const token = localStorage.getItem('authToken');
   return await axios.get(`${API_URL}/search/${userId}`, {
     headers: {
@@ -150,7 +150,7 @@ export const getUserMessages = async () => {
   }
 
   try {
-    const response = await axios.get(`https://rest-api-69mq.onrender.com/attendance/message/${userId}`, {
+    const response = await axios.get(`https://rest-api-hp0n.onrender.com/attendance/message/${userId}`, {
       headers: {
         'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json'
